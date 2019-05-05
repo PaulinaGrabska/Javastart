1dc3f601dec68cc38a85964a29c4eea1f11f4058
pola <paulina.grabska01@gmail.com>
2019-05-05 19:38:25 +0200

    solution of exercises 2.1 and 2.2

--- ex02/src/drinks/Drink.java
+++ ex02/src/drinks/Drink.java
@@ -21,61 +21,13 @@ public class Drink {
     public Drink(String name, double price, boolean withAlcohol, Ingredient ingredient1, Ingredient ingredient2,
                  Ingredient ingredient3){
 
-        setIngredient1(ingredient1);
-        setIngredient2(ingredient2);
-        setIngredient3(ingredient3);
-        setName(name);
-        setPrice(price);
-        setWithAlcohol(withAlcohol);
-
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public boolean isWithAlcohol() {
-        return withAlcohol;
-    }
-
-    public void setWithAlcohol(boolean withAlcohol) {
-        this.withAlcohol = withAlcohol;
-    }
-
-    public Ingredient getIngredient1() {
-        return ingredient1;
-    }
-
-    public void setIngredient1(Ingredient ingredient1) {
         this.ingredient1 = ingredient1;
-    }
-
-    public Ingredient getIngredient2() {
-        return ingredient2;
-    }
-
-    public void setIngredient2(Ingredient ingredient2) {
         this.ingredient2 = ingredient2;
-    }
-
-    public Ingredient getIngredient3() {
-        return ingredient3;
-    }
-
-    public void setIngredient3(Ingredient ingredient3) {
         this.ingredient3 = ingredient3;
+        this.name = name;
+        this.price = price;
+        this.withAlcohol = withAlcohol;
+
     }
+
 }
