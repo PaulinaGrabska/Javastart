4316e8032033d9bf21a87b2fc28e3c7da052fa8a
pola <paulina.grabska01@gmail.com>
2019-05-08 15:57:05 +0200

    Solution of 1st part of exercise 3.1 - without constructor

--- ex03/src/creatingObjects/Desk.java
+++ ex03/src/creatingObjects/Desk.java
@@ -5,14 +5,4 @@ public class Desk{
     int length;
     int width;
     int height;
-/*
-    public Desk(int l, int w, int h){
-
-        length=l;
-        width=w;
-        height=h;
-    }
-*/
-
-
 }
