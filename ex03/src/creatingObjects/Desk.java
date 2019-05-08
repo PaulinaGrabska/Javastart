773be5f6f65dda1e11b015ba7516de3fd4e7bacf
pola <paulina.grabska01@gmail.com>
2019-05-08 15:47:47 +0200

    Creation of folder with exercises from 3rd lesson

---
+++ ex03/src/creatingObjects/Desk.java
@@ -0,0 +1,18 @@
+package creatingObjects;
+
+public class Desk{
+
+    int length;
+    int width;
+    int height;
+/*
+    public Desk(int l, int w, int h){
+
+        length=l;
+        width=w;
+        height=h;
+    }
+*/
+
+
+}
---
+++ ex03/src/creatingObjects/Main.java
@@ -0,0 +1,24 @@
+package creatingObjects;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     Desk desk1 = new Desk();
+     Desk desk2 = new Desk();
+
+     desk1.length = 20;
+     desk1.height = 100;
+     desk1.width = 120;
+
+     desk2.length = 40;
+     desk2.height = 80;
+     desk2.width = 150;
+
+
+        System.out.printf("Dismensions of desk 1 : %dcm x %dcm x %dcm \n",desk1.length, desk1.width, desk1.height);
+        System.out.printf("Dismensions of desk 2 : %dcm x %dcm x %dcm \n",desk2.length, desk2.width, desk2.height);
+
+    }
+
+}
