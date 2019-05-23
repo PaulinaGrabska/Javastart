d290712f8340b91e8e415bdbee3a204d0a33a1fd
pola <paulina.grabska01@gmail.com>
2019-05-23 20:38:09 +0200

    Solution of 7.3 exercise

--- ex07/src/sumAndEvens/Main.java
+++ ex07/src/sumAndEvens/Main.java
@@ -1,4 +1,21 @@
 package sumAndEvens;
-
+/*
+Napisz program, który wyświetla wszystkie liczby parzyste od 0 do 100 w konsoli a na końcu wyświetla dodatkowo ich sumę.
+ */
 public class Main {
+
+    public static void main(String[] args) {
+
+        int sum=0;
+
+        for (int i = 0; i <= 100; i++) {
+            if(i%2==0 && i!=0) {
+                System.out.print(i + " ");
+                sum+=i;
+            }
+        }
+
+        System.out.println("\nSum of even numbers: " + sum);
+
+    }
 }
