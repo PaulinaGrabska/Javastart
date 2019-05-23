72667a2c065bb144d08950ee716c46ca869807bf
pola <paulina.grabska01@gmail.com>
2019-05-23 20:37:29 +0200

    Solution of 7.2 exercise

--- ex07/src/numbers03/Main.java
+++ ex07/src/numbers03/Main.java
@@ -1,4 +1,31 @@
 package numbers03;
-
+/*
+Napisz program, który wyświetli na ekranie liczby z przedziału [0, 3] ze zmianą co 0.1, 
+czyli wynik powinien wyglądać tak: 
+0, 0.1, 0.2, 0.3, …, 3 
+Program napisz w dwóch wersjach z wykorzystaniem pętli while oraz do…while
+ */
 public class Main {
+
+    public static void main(String[] args) {
+/*
+        for (double i = 0; i <3 ; i+=0.1) {
+            System.out.print(i + ", ");
+        }
+*/
+        double i =0;
+        while(i<=3.1){
+            System.out.print(i+", ");
+            i+=0.1;
+        }
+        System.out.println();
+
+        double y=0;
+        do{
+            System.out.print(y+", ");
+            y+=0.1;
+        }while(y<=3.1);
+
+        System.out.println();
+    }
 }
