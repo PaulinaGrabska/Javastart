e2f40219c2e217a511b756cd0f3465fcd922ff93
pola <paulina.grabska01@gmail.com>
2019-05-23 20:37:00 +0200

    Solution of 7.1 exercise

--- ex07/src/welcome/Main.java
+++ ex07/src/welcome/Main.java
@@ -1,4 +1,22 @@
 package welcome;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner user = new Scanner(System.in);
+
+        Welcome welcome = new Welcome();
+
+
+        for (int i = 0; i < welcome.getNames().length ; i++) {
+            System.out.println("Enter " + (i+1) + " name:");
+            String name = user.nextLine();
+            welcome.addName(name);
+        }
+
+        System.out.println("All names in reverse order: ");
+        welcome.sayHi();
+    }
 }
--- ex07/src/welcome/Welcome.java
+++ ex07/src/welcome/Welcome.java
@@ -1,4 +1,39 @@
 package welcome;
-
+/*
+Napisz program, w którym wczytasz od użytkownika 5 imion a następnie wyświetlisz na ekranie powitanie każdej z osób,
+ale w kolejności odwrotnej niż je wprowadzono.
+Np.
+Podaj 5 imion:
+Ania
+Bartek
+Wojtek
+Karol
+Zosia
+Cześć Zosia
+Cześć Karol
+Cześć Wojtek
+Cześć Bartek
+Cześć Ania
+ */
 public class Welcome {
+
+    private String [] names = new String [5];
+    private int nameNumbers;
+
+    public void sayHi(){
+        for (int i = 4; i >=0 ; i--) {
+            System.out.println("Hi " + names[i]);
+        }
+    }
+
+    public void addName(String name){
+        if(nameNumbers<names.length){
+            names[nameNumbers]=name;
+            nameNumbers++;
+        }
+    }
+
+    public String[] getNames() {
+        return names;
+    }
 }
