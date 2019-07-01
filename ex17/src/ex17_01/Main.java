da55f5d6957a75e2ba9ae7ad31729feea16978eb
pola <paulina.grabska01@gmail.com>
2019-07-01 16:25:53 +0200

    ex17_01

--- ex17/src/ex17_01/Main.java
+++ ex17/src/ex17_01/Main.java
@@ -1,4 +1,24 @@
 package ex17_01;
+/*Wczytaj od użytkownika nazwę pory roku (po polsku). Na podstawie tej nazwy
+pobierz odpowiadającą jej wartość typu Season, a na końcu wyświetl miesiące wchodzące w skład danej pory roku.  */
+
+import java.util.Scanner;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj pore roku");
+
+        String s = sc.nextLine();
+
+        System.out.println("Miesiace wchodzace w pore roku " + s + ":");
+
+        for (String m:Season.converter(s)) {
+            System.out.print(m + " ");
+        }
+
+    }
 }
--- ex17/src/ex17_01/Season.java
+++ ex17/src/ex17_01/Season.java
@@ -1,4 +1,36 @@
 package ex17_01;
+/* Każda wartość powinna przechowywać polską nazwę danej pory roku oraz tablicę z miesiącami, które wchodzą
+w skład danej pory roku (załóżmy gru, sty, lut - zima, mar, kwi, maj - wiosna, cze, lip, sie - lato, wrz,
+paz, lis - jesień).  */
 
 public enum Season {
+    SPRING("wiosna", new String[]{"mar", "kwi", "maj"}),
+    SUMMER("lato",new String[]{"cze", "lip", "sie"}),
+    AUTUMN("jesien", new String[]{"wrz","paz","lis"}),
+    WINTER("zima", new String[]{"gru","sty","lut"});
+
+    private String description;
+    private String [] months;
+
+    Season(String description, String[] months) {
+        this.description = description;
+        this.months = months;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String[] getMonths() {
+        return months;
+    }
+
+    static String [] converter(String des){
+        for (Season s:Season.values()) {
+            if(des.equals(s.description)){
+                return s.months;
+            }
+        }
+        return null;
+    }
 }
